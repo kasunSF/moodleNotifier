2521d47b0447a53ed7b57ab3d7e5a71ec70ff99b
kasunSF <sf.kasun@gmail.com>
2014-07-07 21:15:50 +0530

    Options page javascript error fixed

---
+++ js/helper.js
@@ -0,0 +1,7 @@
+function hello(){
+  console.log("click");
+}
+
+function hello2(){
+  console.log("click2");
+}
--- js/options.js
+++ js/options.js
@@ -1,22 +1,40 @@
-function showContent(contentId) {
+//JavaScript for navigate menu in options page
+function showContent(event) {
     $('.content').each(function (index) {
         if (!($(this).hasClass('invisible')))
             $(this).addClass('invisible');
 
-        if (index == contentId)
+        if (index == event.target.id)
             $(this).removeClass('invisible');
     });
 
     $('ul.menu > li > a').each(function (index) {
         $(this).removeClass('active');
 
-        if (index == contentId)
+        if (index == event.target.id)
             $(this).addClass('active');
     });
 }
 
+//JavaScript for set default menu in options page
+function defaultView(contentId) {
+    $('.content').each(function (index) {
+        if (index == contentId)
+            $(this).removeClass('invisible');
+    });
+}
 
-//Preview alert sounds
+//Event listener
+document.addEventListener('DOMContentLoaded', function () {
+  var menu = document.querySelectorAll('li');//Get menu list
+  for (var i = 0; i < menu.length; ++i) {
+    menu[i].addEventListener('click', showContent);//Add event listener to the menu
+  }
+  defaultView(0);//Set default view of the menu: "General settings"
+});
+
+
+
 function playNotificationSound() {
    var source;
 	  source = "sounds/"+document.getElementById("alert").value;//Direct to the location of sound file
